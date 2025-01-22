35d37c90e79bc01480873392fab663f3d5336996
souper GRRR MARICE <soraya.ahmadi@student.tersterre.be>
2025-01-22 09:56:33 +0100

    photo etc

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // JavaScript Document
+var isArti=false;
+var isBms=false;
+var isCnight=false;
+var isDenot=false;
+
+function onClickAa() {
+	document.getElementById("a012").src="images/A rti.jpg"
+	var isArti=true;
+}
+function onClickAb() {
+	document.getElementById("a022").src="images/A rti.jpg"
+	var isArti=true;
+}
+function onClickBa() {
+	document.getElementById("b012").src="images/B ms.avif"
+	var isBms=true;
+}
+function onClickBb() {
+	document.getElementById("b022").src="images/B ms.avif"
+	var isBms=true;
+}
+function onClickCa() {
+	document.getElementById("c012").src="images/C nit.jpg"
+	var isCnight=true;
+}
+function onClickCb() {
+	document.getElementById("c022").src="images/C nit.jpg"
+	var isCnight=true;
+}
+function onClickDa() {
+	document.getElementById("d012").src="images/D enot.avif"
+	var isDenot=true;
+}
+function onClickDb() {
+	document.getElementById("d022").src="images/D enot.avif"
+	var isDenot=true;
+}
+
+
+
+
