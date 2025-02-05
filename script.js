fc24f2ec92c9c2866aa2d198abf6e57e8733d17c
souper GRRR MARICE <soraya.ahmadi@student.tersterre.be>
2025-02-05 09:55:33 +0100

    do

--- script.js
+++ script.js
@@ -1,42 +1,71 @@
-// JavaScript Document
-var isArti=false;
-var isBms=false;
-var isCnight=false;
-var isDenot=false;
+// JavaScript Document 
+//else if (isDenot==false && pressTwice==true)
+//} else if (isDenot==true && pressTwice==false){
+var score="";
+
+console.log=("come into the water")
+
+
+var savedCard = null;
 
 function onClickAa() {
 	document.getElementById("a012").src="images/A rti.jpg"
-	var isArti=true;
+	
+	if(savedCard == null) {
+		savedCard = "a";
+	} else {
+		if(savedCard == "a") {
+			//zelfde -> laten staan
+			savedCard = null;
+		} else{
+			// niet zelfde -> terug omdraaien
+			document.getElementById("a012").src="images/bg.png"
+			
+			savedCard = null;
+		} 
+	} 
 }
 function onClickAb() {
 	document.getElementById("a022").src="images/A rti.jpg"
-	var isArti=true;
+	
+	if(savedCard == null) {
+		savedCard = "a";
+	} else {
+		if(savedCard == "a") {
+			//zelfde -> laten staan
+			
+			savedCard = null;
+		} else{
+			// niet zelfde -> terug omdraaien
+			document.getElementById("a022").src="images/bg.png"
+			
+			savedCard = null;
+		} 
+	}
 }
 function onClickBa() {
 	document.getElementById("b012").src="images/B ms.avif"
-	var isBms=true;
+	
 }
 function onClickBb() {
 	document.getElementById("b022").src="images/B ms.avif"
-	var isBms=true;
+	
 }
 function onClickCa() {
 	document.getElementById("c012").src="images/C nit.jpg"
-	var isCnight=true;
+	
 }
 function onClickCb() {
 	document.getElementById("c022").src="images/C nit.jpg"
-	var isCnight=true;
+	
 }
 function onClickDa() {
 	document.getElementById("d012").src="images/D enot.avif"
-	var isDenot=true;
+	
 }
 function onClickDb() {
-	document.getElementById("d022").src="images/D enot.avif"
-	var isDenot=true;
-}
-
+	
+ }
 
 
 
